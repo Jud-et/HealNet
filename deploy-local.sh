3bfd6897721a67f541ec26781d97fcb799023d2b
Jud-et <jud444h@gmail.com>
2024-09-01 01:33:22 +0300

    chaing to deploy all canisters

--- deploy-local.sh
+++ deploy-local.sh
@@ -1,18 +1,10 @@
+#!/bin/bash
+
 # Installing Dependencies
 npm i
 
 # Starts the replica, running in the background
-dfx start --background
+dfx start --background --clean
 
-# Deploys your Backend canisters to the replica and generates your candid interface
-dfx deploy backend
-
-#Deploy intenet idenity canister
-dfx deploy internet_identity
-
-# Generate Backend Interface for frontend
-npm run generate
-
-# Local frontend development server
-
-npm start
+# Deploys your canisters to the replica and generates your candid interface
+dfx deploy 
